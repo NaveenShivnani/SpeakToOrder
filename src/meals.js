b5e8c4cdf9e2140dfb6e451418a468636702c6f0
Naveen Shivnani <naveenshivnani986@gmail.com>
2019-01-12 23:06:00 +0530

    initial commit

---
+++ src/meals.js
@@ -0,0 +1,30 @@
+export const meals = [
+    {
+        id: 1,
+        name: 'Hamburger',
+        description: 'Juicy beef burger with bacon, fresh tomato, lettuce, mayo, ketchup with side of fries',
+        imgUrl: 'https://images.unsplash.com/photo-1432752641289-a25fc853fceb?ixlib=rb-0.3.5&s=b178e63967b52edba090783eeafd2081&auto=format&fit=crop&w=753&q=80',
+        price: ''
+    },
+    {
+        id: 2,
+        name: 'Pasta',
+        description: 'Pasta with bolognese sauce and parmesan with side of grilled vegetables',
+        imgUrl: 'https://images.unsplash.com/photo-1479796099910-b137a80acde4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b816c2fe3faa1efba2f5dc2c259ac7a5&auto=format&fit=crop&w=704&q=80',
+        price: ''
+    },
+    {
+        id: 3,
+        name: 'Pizza',
+        description: 'Pizza margherita with tomato sauce, fresh italian mozzarella, parmesan and rocket',
+        imgUrl: 'https://images.unsplash.com/photo-1474600056930-615c3d706456?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=7f4bd9ccbf3cb4ac03db0f64fc12f5ae&auto=format&fit=crop&w=752&q=80',
+        price: ''
+    },
+    {
+        id: 4,
+        name: 'Noddles',
+        description: 'Noddles served with steamed vegetables ',
+        imgUrl: 'https://images.unsplash.com/photo-1544886533-62f60554193b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1629&q=80',
+        price: ''
+    }
+]
